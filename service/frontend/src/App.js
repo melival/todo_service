68b2f4c1bfbeaee91fe677ddead0b5a879b98bb3
melival <melival84@gmail.com>
2021-03-14 21:54:31 +0800

    Performed front with hardcore user list

--- service/frontend/src/App.js
+++ service/frontend/src/App.js
@@ -1,25 +1,48 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './components/User.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      'users': []
+    }
+  }
+
+  componentDidMount() {
+    const users = [
+      {
+        'user_name': "username1",
+        'first_name': "first1",
+        'last_name': "last",
+        'email': "mail1@mail.ru"
+      },
+      {
+        'user_name': "username2",
+        'first_name': "first2",
+        'last_name': "last2",
+        'email': "mail2@mail.ru"
+      }
+    ]
+    this.setState(
+      {
+        'users': users
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <UserList users={this.state.users} />
+
+      </div>
+    )
+  }
 }
 
 export default App;
